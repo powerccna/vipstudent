04ba0f1c95ae8f24c5395a19b26a6b27ee44af48
yinjiwan <yinjiwan@meituan.com>
2019-04-27 00:23:49 +0800

    add

---
+++ src/mytest/test_dic.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+#coding=utf-8
+
+dic ={'a':1,'b':2}
+
+for k, v in dic.items():
+    print(k,v)
+
+for v in dic.values():
+    print(v)
+
+for v in dic.keys():
+    print(v)
